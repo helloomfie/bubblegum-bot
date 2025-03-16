3fecb22494043f362509c9295b637324c7919d73
bubblegum <oomfiedev@gmail.com>
2025-03-16 18:03:56 -0400

    Update bot_trainer.py

--- bot_trainer.py
+++ bot_trainer.py
@@ -4,35 +4,34 @@ from sklearn.pipeline import Pipeline
 
 class BubblegumBotTrainer:
     def __init__(self):
-        # Create a machine learning pipeline
+
         self.model = Pipeline([
             ('vectorizer', TfidfVectorizer()),
             ('classifier', MultinomialNB())
         ])
         
         self.training_data = [
-            # Your existing dataset goes here
+
         ]
     
     def train(self):
-        # Prepare training data
+
         X = [item['input'] for item in self.training_data]
         y = [item['intent'] for item in self.training_data]
         
-        # Train the model
+
         self.model.fit(X, y)
     
     def predict_intent(self, message):
-        # Predict the intent of a message
+
         return self.model.predict([message])[0]
     
     def get_response(self, message):
-        # Determine intent and select appropriate response
+
         intent = self.predict_intent(message)
         matching_responses = [
             item['response'] for item in self.training_data 
             if item['intent'] == intent
         ]
-        
-        # Randomly select a response
+
         return random.choice(matching_responses) if matching_responses else "whatever."
